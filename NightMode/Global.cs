4527b530fe81c54f74e28a79b0fe903a0af5c6c3
Sandeep Bansal <s@ndeep.me>
2015-06-17 23:44:46 +0100

    Initial Commit

---
+++ NightMode/Global.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightMode
+{
+    public static class Global
+    {
+
+        private static ThemeModel themeModel;
+
+        public static ThemeModel ThemeModel
+        {
+            get { return themeModel; }
+            set { themeModel = value; }
+        }
+
+        public static void LightTheme()
+        {
+            ThemeModel.ThemeName = "Light";
+            ThemeModel.BackgroundColour = "#EEEEEE";
+            ThemeModel.HeaderColour = "#BBBBBB";
+            ThemeModel.ForegroundColour = "#000000";
+        }
+
+        public static void DarkTheme()
+        {
+            ThemeModel.ThemeName = "Dark";
+            ThemeModel.BackgroundColour = "#333333";
+            ThemeModel.HeaderColour = "#000000";
+            ThemeModel.ForegroundColour = "#FFFFFF";
+        }
+
+    }
+}
---
+++ NightMode/ThemeModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightMode
+{
+    public class ThemeModel : INotifyPropertyChanged
+    {
+        private string _themeName;
+        private string _backgroundColour;
+        private string _foregroundColour;
+        private string _headerColour;
+
+        public string ThemeName
+        {
+            get { return _themeName; }
+            set { _themeName = value; OnPropertyChanged("ThemeName"); }
+        }
+
+        public string BackgroundColour
+        {
+            get { return _backgroundColour; }
+            set { _backgroundColour = value; OnPropertyChanged("BackgroundColour"); }
+        }
+
+        public string ForegroundColour
+        {
+            get { return _foregroundColour; }
+            set { _foregroundColour = value; OnPropertyChanged("ForegroundColour"); }
+        }
+
+        public string HeaderColour
+        {
+            get { return _headerColour; }
+            set { _headerColour = value; OnPropertyChanged("HeaderColour"); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
